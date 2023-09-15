8a5b3928b1d676c8071b0096932f14901f3d2e63
oreoanytime <adrianustw.2021@scis.smu.edu.sg>
2023-09-15 23:11:17 +0800

    fixed jobs.py

--- jobs.py
+++ jobs.py
@@ -2,10 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from urllib.parse import quote
-import os
-import datetime, time
-import uuid
-import base64
+from bson import ObjectId
+import json, os
+from datetime import datetime, time
 import pymongo
 
 load_dotenv()
@@ -15,7 +14,7 @@ encoded_password = quote(password)
 connection_string = f"mongodb+srv://g1t7-is212-dev:{encoded_password}@sbrp.wtds9l8.mongodb.net/?retryWrites=true&w=majority" 
 
 client = pymongo.MongoClient(connection_string)
-db = client["g1t7-is212-dev"]
+db = client["skills_based_role_portal"]
 
 try:
     # List available databases (a simple operation to check connection)
@@ -28,6 +27,15 @@ except Exception as e:
 app = Flask(__name__)
 CORS(app)
 
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)  # Convert ObjectId to a string
+        if isinstance(obj, datetime):
+            return obj.isoformat()  # Convert datetime to ISO format
+        return super(CustomEncoder, self).default(obj)
+    
+
 @app.route("/jobs", methods=['GET'])
 def get_all_jobs():
     jobs_collection_ref = db.get_collection('job_listings')
@@ -37,11 +45,11 @@ def get_all_jobs():
         jobs = []
 
         for job in job_doc_ref:
-            job_dict = job  
-            job_dict['jobid'] = str(job['_id']) 
+            job_json = json.dumps(job, cls=CustomEncoder, indent=4)
+            job_dict = json.loads(job_json)  
+            job_dict['jobid'] = str(job['_id'])
             jobs.append(job_dict)
-
-
+            
         if len(jobs) == 0:
             return jsonify(
                 {
@@ -61,11 +69,11 @@ def get_all_jobs():
     except Exception as e:
         return jsonify(
             {
-                "code": 404,
+                "code": 500,
                 "message": f"Error retrieving jobs: {str(e)}"
             }
         )
-    
+
 
 #####################################
 # RUN SCRIPT
