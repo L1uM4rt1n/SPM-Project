1607e60178d02205fad888806425f39b9c6c3638
oreoanytime <adrianustw.2021@scis.smu.edu.sg>
2023-09-15 21:11:35 +0800

    initialize mongodb

--- jobs.py
+++ jobs.py
@@ -1,20 +1,31 @@
-from flask import Flask, request, jasonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+from urllib.parse import quote
+import os
 import datetime, time
 import uuid
 import base64
 import pymongo
 
-mongo_host =
-mongo_port = 27017
-mongo_database = 
+password = os.getenv("MONGODB_PASSWORD").encode()
+encoded_password = quote(password)
+connection_string = f"mongodb+srv://g1t7-is212-dev:{password}@sbrp.wtds9l8.mongodb.net/?retryWrites=true&w=majority" 
 
-client = pymongo.MongoClient(host=mongo_host, port=mongo_port)
-db = client[mongo_database]
+client = pymongo.MongoClient(connection_string)
+db = client["g1t7-is212-dev"]
+
+try:
+    # List available databases (a simple operation to check connection)
+    database_list = client.list_database_names()
+    print("Connection to MongoDB established successfully.")
+    print("Available databases:", database_list)
+except Exception as e:
+    print("Connection to MongoDB failed:", e)
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/jobs", methods=['GET'])
 def get_all_jobs():
-    
+    return 1
