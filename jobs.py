65e3de77c4152f908c04bc65e24a55d9f2bf3ad3
oreoanytime <adrianustw.2021@scis.smu.edu.sg>
2023-09-15 22:16:46 +0800

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -8,9 +8,11 @@ import uuid
 import base64
 import pymongo
 
+load_dotenv()
+
 password = os.getenv("MONGODB_PASSWORD").encode()
 encoded_password = quote(password)
-connection_string = f"mongodb+srv://g1t7-is212-dev:{password}@sbrp.wtds9l8.mongodb.net/?retryWrites=true&w=majority" 
+connection_string = f"mongodb+srv://g1t7-is212-dev:{encoded_password}@sbrp.wtds9l8.mongodb.net/?retryWrites=true&w=majority" 
 
 client = pymongo.MongoClient(connection_string)
 db = client["g1t7-is212-dev"]
@@ -28,4 +30,46 @@ CORS(app)
 
 @app.route("/jobs", methods=['GET'])
 def get_all_jobs():
-    return 1
+    jobs_collection_ref = db.get_collection('job_listings')
+    
+    try:
+        job_doc_ref = jobs_collection_ref.find()
+        jobs = []
+
+        for job in job_doc_ref:
+            job_dict = job  
+            job_dict['jobid'] = str(job['_id']) 
+            jobs.append(job_dict)
+
+
+        if len(jobs) == 0:
+            return jsonify(
+                {
+                    "code": 404,
+                    "message": "No jobs found"
+                }
+            )
+        
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "jobs": jobs
+                } 
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 404,
+                "message": f"Error retrieving jobs: {str(e)}"
+            }
+        )
+    
+
+#####################################
+# RUN SCRIPT
+#####################################
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5007, debug=True)
