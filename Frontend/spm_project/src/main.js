613282bccac9601a2bac8bcb1885120e4b9157e0
oreoanytime <adrianustw.2021@scis.smu.edu.sg>
2023-09-16 14:46:08 +0800

    minor fixes

--- Frontend/spm_project/src/main.js
+++ Frontend/spm_project/src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/router'
-
 import '@fortawesome/fontawesome-free/js/all'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+// Use FontAwesome
+app.config.globalProperties.$fontawesome = require('@fortawesome/fontawesome-svg-core')
+
+// Use BootstrapVue
+app.use(router)
+app.use(BootstrapVue3)
+
+app.mount('#app')
