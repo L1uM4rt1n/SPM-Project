dbcd1d1bac9d2aa00957663338a53dfe084bf55c
oreoanytime <111495669+oreoanytime@users.noreply.github.com>
2023-09-19 14:58:54 +0800

    Frontend Search Bar Changes

--- Frontend/spm_project/src/components/SearchBar.vue
+++ Frontend/spm_project/src/components/SearchBar.vue
@@ -4,87 +4,35 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <div class="input-group" id="adv-search">
-            <input type="text" class="form-control " placeholder="Search for job listings" />
-            <div class="input-group-btn">
-              <div class="btn-group" role="group">
-                <div class="dropdown dropdown-lg">
-                  <button type="button" class="btn btn-default dropdown-toggle border-bottom border-top"
-                    data-toggle="dropdown" aria-expanded="false"><span class="caret"></span></button>
-                  <div class="dropdown-menu dropdown-menu-right" role="menu">
-                    <form class="form-horizontal" role="form">
-                      <div class="form-group">
-                        <label for="filter">Filter by</label>
-                        <select class="form-control">
-                          <option value="0" selected>All departments</option>
-                          <option value="1">IT</option>
-                          <option value="2">Finance</option>
-                          <option value="3">HR</option>
-                          <option value="4">Marketing</option>
-                        </select>
-                      </div>
-                      <div class="form-group">
-                        <label for="contain">Author</label>
-                        <input class="form-control" type="text" />
-                      </div>
-                      <div class="form-group">
-                        <label for="contain">Contains the skills</label>
-                        <input class="form-control" type="text" placeholder="Use ; as separator" />
-                      </div>
-
-                      <div class="form-group dropdown">
-                        <select class="select" multiple data-mdb-placeholder="Example placeholder">
-                          <option value="1">One</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                          <option value="4">Four</option>
-                          <option value="5">Five</option>
-                        </select>
-                      </div>
-
-                      <div class="dropdown" data-control="checkbox-dropdown">
-                        <label class="dropdown-label"><b>Select Skills</b></label>
-
-                        <div class="dropdown-list">
-                          <a href="#" data-toggle="check-all" class="dropdown-option">
-                            Check All
-                          </a>
-
-                          <label class="dropdown-option">
-                            <input type="checkbox" name="dropdown-group" value="Selection 1" />
-                            Web development
-                          </label>
-
-                          <label class="dropdown-option">
-                            <input type="checkbox" name="dropdown-group" value="Selection 2" />
-                            Excel
-                          </label>
-
-                          <label class="dropdown-option">
-                            <input type="checkbox" name="dropdown-group" value="Selection 3" />
-                            Python
-                          </label>
-
-                          <label class="dropdown-option">
-                            <input type="checkbox" name="dropdown-group" value="Selection 4" />
-                            Powerpoint
-                          </label>
-
-                          <label class="dropdown-option">
-                            <input type="checkbox" name="dropdown-group" value="Selection 5" />
-                            Java
-                          </label>
-                        </div>
-                      </div>
-
-                    </form>
-                  </div>
-                </div>
-                <button type="button" class="btn btn-outline-primary">
-                  <i class="bi bi-search"></i> Search
-                </button>
+          <form class="d-flex">
+            <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" :aria-controls="'collapseExample'">
+              <i class="fa-solid fa-filter"></i>
+            </button>
+            <button class="btn btn-outline-primary" type="submit">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
+          <div class="collapse" id="collapseExample">
+            <form>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Filter by</label>
+                <select class="form-select" aria-label="Default select example">
+                  <option selected>All Departments</option>
+                  <option value="1">Finance</option>
+                  <option value="2">HR</option>
+                  <option value="3">IT</option>
+                </select>
               </div>
-            </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Author</label>
+                <input type="text" class="form-control" placeholder="Author">
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Contain Skills</label>
+                <input type="formtags" class="form-control" data-role="tagsinput" placeholder="Use ; as separator">
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -93,14 +41,8 @@
 </template>
 
 <script>
-// import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JavaScript
-// import $ from 'jquery'; // Import jQuery
-// import 'bootstrap/dist/css/bootstrap.css';
+
 export default {
-  // mounted() {
-  //   // Initialize the Bootstrap dropdown when the component is mounted
-  //   $(this.$refs.dropdown).dropdown();
-  // },
   data() {
     return {
       searchText: '',
@@ -114,51 +56,11 @@ export default {
 </script>
 
 <style scoped>
-.dropdown.dropdown-lg .dropdown-menu {
-  margin-top: -1px;
-  padding: 6px 20px;
+.collapse {
+  margin-top: 2em;
+  padding: 5px 10px;
+  border: 1px solid grey;
+  border-radius: 10px;
+  text-align: left;
 }
-
-.input-group-btn .btn-group {
-  display: flex !important;
-}
-
-.btn-group .btn {
-  border-radius: 0;
-  margin-left: -1px;
-}
-
-.btn-group .btn:last-child {
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
-}
-
-.btn-group .form-horizontal .btn[type="submit"] {
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-}
-
-.form-horizontal .form-group {
-  margin-left: 0;
-  margin-right: 0;
-}
-
-.form-group .form-control:last-child {
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-}
-
-@media screen and (min-width: 768px) {
-  #adv-search {
-    width: 500px;
-    margin: 0 auto;
-  }
-
-  .dropdown.dropdown-lg {
-    position: static !important;
-  }
-
-  .dropdown.dropdown-lg .dropdown-menu {
-    min-width: 500px;
-  }
-}</style>
+</style>
--- Frontend/spm_project/src/main.js
+++ Frontend/spm_project/src/main.js
@@ -2,9 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/router'
 import '@fortawesome/fontawesome-free/js/all'
-import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 const app = createApp(App)
 
@@ -13,6 +12,5 @@ app.config.globalProperties.$fontawesome = require('@fortawesome/fontawesome-svg
 
 // Use BootstrapVue
 app.use(router)
-app.use(BootstrapVue3)
 
 app.mount('#app')
