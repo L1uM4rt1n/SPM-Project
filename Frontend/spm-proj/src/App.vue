aed36258c19c7a06a989fa27d15927ff7d5da86c
ItsPeeko <111348072+ItsPeeko@users.noreply.github.com>
2023-09-22 19:11:43 +0800

    shadow for navbar

--- Frontend/spm-proj/src/App.vue
+++ Frontend/spm-proj/src/App.vue
@@ -8,7 +8,7 @@
 
 
     <!-- Navigation Bar content-->
-    <nav class="navbar navbar-light border-bottom border-dark">
+    <nav class="navbar navbar-light border-bottom border-dark" style="box-shadow: 0px 5px 5px grey;">
       <div class="container-fluid">
         <router-link :to="{ name: 'landingPage' }">
         <a class="navbar-brand" href="#">
