ee30cbbff821203a39a5d192b383e2856c326261
ItsPeeko <111348072+ItsPeeko@users.noreply.github.com>
2023-09-22 18:12:37 +0800

    copy shanmei stuff

---
+++ Frontend/spm-proj/src/components/AppNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+            <img src="../components/img/ondeh.jpeg" width="50" height="50" alt="">  <i>Ondeh Ondeh</i>
+            </a>
+            <a class="navbar-brand" href="#">
+            <img src="../components/img/user.png"  class="rounded-circle" width="50" height="50" alt="">
+            </a>
+        </div>
+        </nav>
+    </template>
+    
+<script>
+import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap 4 CSS
+import 'jquery/dist/jquery.min.js'; // Import jQuery
+import 'bootstrap/dist/js/bootstrap.min.js'; // Import Bootstrap 4 JS
+
+export default {
+    name: 'AppNavbar',
+};
+
+</script>
+
+<style>
+/* Add component-specific styles here */
+</style>
+    
