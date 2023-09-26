a692b2cfc544ea296adebf509fff19f46161246a
oreoanytime <111495669+oreoanytime@users.noreply.github.com>
2023-09-26 17:37:28 +0800

    Small changes
    Added item card component
    Edit homepage.vue in view folder

---
+++ Frontend/spm_project/src/components/ItemCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <router-link
+    ::to="{ path: '/roleinfo', query: { listingID: listingData.listingid} }"
+    style="text-decoration: none; color: inherit;">
+    <div class="card">
+        <div class="card-header">
+          Featured
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>   
+    </router-link>
+</template>
+
+<script>
+    export default {
+    name: 'ItemCard',
+    props: {
+        listingData: null,
+        mylistings: {
+        type: Boolean,
+        default: false
+        }
+    }
+</script>
--- Frontend/spm_project/src/views/HomePage.vue
+++ Frontend/spm_project/src/views/HomePage.vue
@@ -1,5 +1,17 @@
 <template>
     <SearchBar> </SearchBar>
+    <div>
+      <h1 class="pb-3">Role Listings</h1>
+      <div class="row d-flex mx-2">
+        <ItemCard v-for="listing in this.filteredListings" :listingData="listing"
+          v-if="filteredListings && filteredListings.length > 0" :uid="user_id" />
+        <div class="container-fluid d-flex justify-content-center align-items-center"
+          style="height: calc(100vh - 200px); overflow: hidden;" v-else-if="searchInput">
+          <p class="fs-5" style="color: #C6C6C6">No results found.</p>
+        </div>
+        <ItemCard v-for="listing in this.listings" :listingData="listing" v-else :uid="user_id" />
+      </div>
+    </div>
 
 </template>
 
