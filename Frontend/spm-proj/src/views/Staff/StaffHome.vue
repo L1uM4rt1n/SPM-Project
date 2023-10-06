b56e91598a74740c4e7bd9e9b237844927fac55e
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2023-10-06 23:54:35 +0800

    home page integration up

--- Frontend/spm-proj/src/views/Staff/StaffHome.vue
+++ Frontend/spm-proj/src/views/Staff/StaffHome.vue
@@ -1,80 +1,57 @@
 <template>
-    <div>
-        <component :is="searchBarComponent" @search-request="performSearch" />
+    <div class="app">
+        <!-- dynamic search bar -->
+
+        <!-- display of all roles -->
         <div class="container">
-            <router-link to="'/role/' + role.slug" class="card-link" v-for="role in roles" :key="role.id">
-                <div class="card rounded" style="border: 2px solid #ccc;">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ role.role_name }}</h4>
-                        <h6 class="card-text">Role ID: {{ role.role_id }}</h6>
-                        <p class="card-text">Availability: {{ role.availability }}</p>
-                        <p class="card-text">Application Deadline: {{ role.application_deadline }}</p>
-                        <component :is="skillMatchPercentageComponent" :role-skills="role.skills" :staff-skills="staffSkills" />
-                    </div>
+            <div class="card rounded" style="border: 2px solid #ccc;" v-for="role in roles" :key="role.Role_ID">
+                <div class="card-body">
+                    <h4 class="card-title">{{ role.Role_Name }}</h4>
+                    <p class="card-text">Role ID: {{ role.Role_ID }}</p>
+                    <p class="card-text">Application Deadline: {{ role.App_Deadline }}</p>
                 </div>
-            </router-link>
+            </div>
         </div>
+
+        <!-- tester -->
+        <ul>
+            <li v-for="role in roles" :key="role.role_id">
+                {{ role.role_name }}
+            </li>
+        </ul>
     </div>
 </template>
 
 
 <script>
-    import axios from 'axios';
-    import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap 4 CSS
-    import 'jquery/dist/jquery.min.js'; // Import jQuery
-    import 'bootstrap/dist/js/bootstrap.min.js'; // Import Bootstrap 4 JS
+    import axios from 'axios'
+    import { server } from "../../utils/helper.js"
+    import 'bootstrap/dist/css/bootstrap.css'
+    import 'jquery/dist/jquery.min.js'
+    import 'bootstrap/dist/js/bootstrap.min.js'
 
     export default {
         name: 'StaffHome',
         data() {
             return {
                 roles: [],
-                staffSkills: [],
-                searchBarComponent: 'SearchBar',
-                skillMatchPercentageComponent: null,
             };
         },
         created() {
-            this.getRoles();
-            this.getStaffSkills();
-            this.loadSkillMatchPercentageComponent();
+            this.getAllRoles();
         },
         methods: {
-            getRoles() {
-                axios.get('/api/roles')
-                    .then(response => {
-                        this.roles = response.data.map(role =>  ({
-                            role_name: role.role_name,
-                            role_id: role.role_id,
-                            availability: role.availability,
-                            application_deadline: role.application_deadline,
-                        }))
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            getStaffSkills() {
-                axios.get('/api/staff/skills')
-                    .then(response => {
-                        this.staffSkills = response.data;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            loadSkillMatchPercentageComponent() {
-                import('../../components/SkillsMatchedPercentage.vue')
-                    .then(module => {
-                        this.skillMatchPercentageComponent = module.default;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            performSearch(searchParams) {
-                // perform search based on searchParams
-                console.log(searchParams);
+            getAllRoles() {
+                axios.get(`${server.baseURL}/roles/get_all_roles`)
+                    .then(
+                        (response) => {
+                            // console.log(response)
+                            this.roles = response.data.data.bookings
+                        }
+                    )
+                    // .catch((error) => {
+                    //     console.error(error)
+                    // });
             },
         },
     };
