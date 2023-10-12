8ad8a25b36e70faf6836dd2e227223d3a52749c6
ItsPeeko <111348072+ItsPeeko@users.noreply.github.com>
2023-10-12 11:11:25 +0800

    pushing to update my backend endpoints (nth updated)

--- Frontend/spm-proj/src/views/createJobListing.vue
+++ Frontend/spm-proj/src/views/createJobListing.vue
@@ -9,57 +9,39 @@
         <div class="container">
             <div class="form-group">
                 <label for="exampleInputEmail1">Name</label>
-                <input type="email" class="form-control" id="jobName" aria-describedby="jobName" placeholder="Enter Role Listing Name" style="margin-bottom: 15px;">
+                <input v-model="NewRoleName" type="email" class="form-control" id="jobName" aria-describedby="jobName" placeholder="Enter Role Listing Name" style="margin-bottom: 15px;">
                 <!-- <small id="" class="form-text text-muted">This is a small description</small> -->
             </div>
 
-
             <div>
                 Application Deadline <br>
-                <input type="date" id="myDate" value="2023-10-09">
+                <input v-model="NewDeadline" type="date" id="myDate">
+                {{NewDeadline}}
 
             </div>
 
             <div>
                 <label class="mr-2" for="inlineFormCustomSelectPref" style="margin-top: 15px;">Department</label>
                 <div>
-                    <select v-model="selectedDepartments" class="custom-select my-1 mr-sm-2 mb-4" id="inlineFormCustomSelectPref">
+                    <select v-model="NewDepartment" class="custom-select my-1 mr-sm-2 mb-4" id="inlineFormCustomSelectPref">
                         <option disabled selected>Choose Department...</option>
-                        <option>New</option>
-                        <option v-for="job in filteredResults" :key="job" :value="job">{{ job.Role_Department }}</option>
+                        <option v-for="job in filteredResults" :key="job" :value="job.Role_Department">{{ job.Role_Department }}</option>
                     </select>
                 </div>
 
-                <input v-if="selectedDepartments === 'New'" type="text" placeholder="New Department Name" style="width: 100%;">
             </div>
             
             <div>
                 <label for="exampleFormControlTextarea1">Role Description</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+                <textarea v-model="NewDescription" class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
             </div>
 
             <div>
-                <label for="exampleFormControlTextarea1" style="margin-top: 15px;">Role Expectations</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
-            </div>
-
-            <!-- Checkboxes -->
-            <div class="py-3">
-                <label for="Skills">Skills Required</label><br>
-                <div v-for="skill in skilltypes" :key="skill" class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" :key="skill" :value="skill">
-                    <label class="form-check-label" for="inlineCheckbox1">{{skill}}</label>
-                </div>
-                    
-                <!-- Additional 'Other' checkbox -->
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="otherSkill" v-model="isOtherChecked">
-                    <label class="form-check-label" for="otherSkill">Other</label>
-                </div>
+                <label for="exampleFormControlTextarea1" style="margin-top: 15px;">Role Requirements</label>
+                <textarea v-model="NewRequirements" class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
             </div>
             <br>
-            <textarea v-if="isOtherChecked" placeholder="Please specify the other skill(s)" style="width: 100%;"></textarea>
-            <br>
+            <button class="btn btn-success" v-on:click="createrole">Create</button>
 
         </div>
     </div>
@@ -80,16 +62,15 @@ export default {
     // Other component options and code
 data() {
         return {
-        selectedSkills: [],
-        checkedSkills: [],  // This will store checked skills
-        isOtherChecked: false,  // This will track if 'Other' is checked
-        selectedDepartments: [],
-        selectedDepartment: '',
-        searchKeyword: '', // Add a data property for search keyword
-        jobListings: {}, // Initialize an empty array for job listings
-        jobtypes: [], // All job types
-        skilltypes: [], // All skill types
-        filteredResults:[],
+            filteredResults:[], //response.data.data.bookings is here
+            searchKeyword: '', // Add a data property for search keyword
+            NewRoleName: '', // the new role name
+            NewDepartment: '', //the new department
+            NewDescription: '', // the new Role Description
+            NewRequirements: '', // the new Role Requirements
+            NewDeadline: '', // the new Deadline
+            TodaysDate: new Date(),
+
         };
     },
     methods: {
@@ -97,58 +78,56 @@ data() {
             axios.get("http://127.0.0.1:5000/roles/get_all_roles")
             .then(response => {
                 this.filteredResults = response.data.data.bookings
-                //console.log('----FilteredResults-----')
-                //console.log(this.filteredResults)
-
-
-                for(let i = 0; i < this.filteredResults.length; i++){
-                    //console.log(this.filteredResults[i])
-                    this.jobListings[i] = this.filteredResults[i]
-                }
-
-                //console.log('----jobListing-----')
-                //console.log(this.jobListings[0])
-
-                let departmentss = ['New'];
-                for (let i = 0; i < this.jobListings.length; i++) {
-                    let dept = this.jobListings[i].Role_Department;
-                    console.log('1');
-                    if (!departmentss.includes(dept)) {
-                        departmentss.push(dept);
-                }
-
-                this.jobtypes = departmentss;
-
-                console.log(this.jobtypes)
-            }
-                
-
+                console.log(this.filteredResults)
             })
             .catch(error => {
                 console.error("There was an error fetching the data:", error);
             });
         },
-        populating() {
-            let departmentss = ['New'];
-            console.log(this.jobListings)
-            for (let i = 0; i < this.jobListings.length; i++) {
-                let dept = this.jobListings[i].Role_Department;
-                console.log('1');
-                if (!departmentss.includes(dept)) {
-                    departmentss.push(dept);
-                }
-            }
-            this.jobtypes = departmentss;
+        test() {
+            console.log(this.NewDeadline)
+            console.log(this.NewDepartment)
+            console.log(this.NewDescription)
+            console.log(this.NewRequirements)
+            console.log(this.NewRoleName)
+            console.log(this.TodaysDate)
+        },
+        createrole() {
+            const inputDateString = this.NewDeadline
 
+            // Create a Date object from the input date string
+            const date = new Date(inputDateString);
 
-            let skills = [];
-            for (let i = 0; i < this.jobListings.length; i++) {
-            skills = skills.concat(this.jobListings[i].skills);
-            }
+            // Create an array of day names and month names
+            const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-            // Remove duplicates by converting the array to a Set and then back to an array
-            this.skilltypes = [...new Set(skills)];
-            },
+            // Format the date in the desired format
+            const formattedDate = `${dayNames[date.getDay()]} ${monthNames[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+
+            console.log(inputDateString)
+            // Prepare the role data to send in the request body
+            const roleData = {
+            Role_Name: this.NewRoleName, // Replace with the actual data
+            Date_Posted: this.TodaysDate, // Replace with the actual data
+            App_Deadline: formattedDate, // Replace with the actual data
+            Role_Department: this.NewDepartment, // Replace with the actual data
+            Role_Description: this.NewDescription, // Replace with the actual data
+            Role_Requirements: this.NewRequirements, // Replace with the actual data
+
+            };
+
+            // Send the PUT request
+            axios.put('/roles/create', roleData)
+            .then(response => {
+                // Handle the successful response here
+                console.log('Role created:', response.data);
+            })
+            .catch(error => {
+                // Handle any errors here
+                console.error('Error creating role:', error);
+            });
+        },
     performSearch(payload) {
         if (payload) {
         const { keyword, selectedDepartments, selectedSkills } = payload;
@@ -183,7 +162,6 @@ data() {
     mounted() {
         // Use axios to fetch data from Flask API
         this.fetchroles();
-        this.populating();
     },
     };
 </script>
