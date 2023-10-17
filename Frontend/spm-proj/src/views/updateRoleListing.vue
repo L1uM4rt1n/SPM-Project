fb2310136221057df968421ef49f84e98ae03948
jaymelek <112159350+jaymelek@users.noreply.github.com>
2023-10-17 15:01:49 +0800

    copy backend

---
+++ Frontend/spm-proj/src/views/updateRoleListing.vue
@@ -0,0 +1,70 @@
+<template>
+
+    <div class="updateRoleListing container my-5">
+    <div class="text-center">
+        <h1 class="">Update Role Listing</h1>
+    </div>
+    
+    <hr>
+        <div class="container">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Name</label>
+                <input type="email" class="form-control" id="jobName" aria-describedby="jobName" placeholder="Enter Role Listing Name" style="margin-bottom: 15px;">
+            </div>
+
+
+            <div>
+                Application Deadline <br>
+                <input type="date" id="myDate" value="2023-10-09">
+
+            </div>
+
+            <div>
+                <label class="mr-2" for="inlineFormCustomSelectPref" style="margin-top: 15px;">Department</label>
+                <div>
+                    <select v-model="selectedDepartments" class="custom-select my-1 mr-sm-2 mb-4" id="inlineFormCustomSelectPref">
+                        <option disabled selected>Choose Department...</option>
+                        <option v-for="type in jobtypes" :key="type" :value="type">{{ type }}</option>
+                    </select>
+                </div>
+
+                <input v-if="selectedDepartments === 'New'" type="text" placeholder="New Department Name" style="width: 100%;">
+            </div>
+            
+            <div>
+                <label for="exampleFormControlTextarea1">Role Description</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+            </div>
+
+            <div>
+                <label for="exampleFormControlTextarea1" style="margin-top: 15px;">Role Expectations</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+            </div>
+
+            <!-- Checkboxes -->
+            <div class="py-3">
+                <label for="Skills">Skills Required</label><br>
+                <div v-for="skill in skilltypes" :key="skill" class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" :key="skill" :value="skill">
+                    <label class="form-check-label" for="inlineCheckbox1">{{skill}}</label>
+                </div>
+                    
+                <!-- Additional 'Other' checkbox -->
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="otherSkill" v-model="isOtherChecked">
+                    <label class="form-check-label" for="otherSkill">Other</label>
+                </div>
+            </div>
+            <br>
+            <textarea v-if="isOtherChecked" placeholder="Please specify the other skill(s)" style="width: 100%;"></textarea>
+            <br>
+            <router-link :to="{ name: 'createSuccess' }">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </router-link>
+        </div>
+    </div>
+
+    
+
+
+</template>
