5812e939798e2c7ad8e8e2c2605824798357954b
ItsPeeko <111348072+ItsPeeko@users.noreply.github.com>
2023-10-24 19:38:56 +0800

    login page

--- Frontend/spm-proj/src/views/profile.vue
+++ Frontend/spm-proj/src/views/profile.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>
+      <div class="createJobListing container my-5">
+    <div class="text-center">
+        <h1 class="">Your Profile</h1>
+    </div>
     
-  </div>
+    <hr>
+        <div class="container">
+            <div>
+                <span style="font-weight:bold" >Name: </span>Johnny 
+                <span style="font-weight:bold">Department: </span>Johnny
+                
+            </div>
+
+            <div>
+                <span style="font-weight:bold">Country: </span>Johnny
+                <span style="font-weight:bold">Email: </span>Johnny
+            </div>
+
+            <br>
+
+            <div>
+                <span style="font-weight:bold">Skills: </span>
+            </div>
+            
+            <br>
+            
+            <div>
+                <span style="font-weight:bold">Jobs Applied: </span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
