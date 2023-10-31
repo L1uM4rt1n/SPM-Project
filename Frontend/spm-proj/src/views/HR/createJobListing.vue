b53378cabf182492e517826b240a3c264c0577f1
ItsPeeko <111348072+ItsPeeko@users.noreply.github.com>
2023-10-31 19:21:27 +0800

    create role listing updated to new end points

--- Frontend/spm-proj/src/views/HR/createJobListing.vue
+++ Frontend/spm-proj/src/views/HR/createJobListing.vue
@@ -24,14 +24,8 @@
                 <div>
                     <select v-model="NewDepartment" class="custom-select my-1 mr-sm-2 mb-4" id="inlineFormCustomSelectPref">
                         <option disabled selected>Choose Department...</option>
-                        <option v-for="job in filteredResults" :key="job" :value="job.Role_Department">{{ job.Role_Department }}</option>
+                        <option v-for="job in filteredResults2" :key="job" :value="job">{{ job }}</option>
                     </select>
-                </div>
-
-                
-                <div>
-                    <label for="inlineFormCustomSelectPref" style="margin-top: 15px;">Availability</label>
-                    <textarea v-model="NewAvailability" class="form-control" ></textarea> 
                 </div>
 
             </div>
@@ -42,14 +36,15 @@
             </div>
 
             <div>
-                <label for="exampleFormControlTextarea1" style="margin-top: 15px;">Role Requirements</label>
-                <textarea v-model="NewRequirements" class="form-control" rows="6" ></textarea>
+                <label for="exampleFormControlTextarea1" style="margin-top: 15px;">Role Skills</label>
+                <div class="checkboxes">
+                    <label v-for="skill in allskills" :key="skill">
+                        <input type="checkbox" v-model="selectedSkills" :value="skill"> {{ skill }}
+                    </label>
+                </div>
+                {{ selectedSkills }}
             </div>
 
-            <div>
-                <label for="exampleFormControlTextarea1" style="margin-top: 15px;" >Role Skills</label>
-                <textarea v-model="NewRoleSkills" class="form-control" rows="6" placeholder='Enter All Skills, Each Seperated By A Comma' style='text-align:center'></textarea>
-            </div>
 
             <br>
             <button class="btn btn-success" v-on:click="createrole">Create</button>
@@ -63,129 +58,103 @@
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap 4 CSS
-import 'jquery/dist/jquery.min.js'; // Import jQuery
-import 'bootstrap/dist/js/bootstrap.min.js'; // Import Bootstrap 4 JS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'jquery/dist/jquery.min.js';
 import axios from 'axios';
-
-
-
-
+import { server } from "../../utils/helper.js";
 
 export default {
-    name: 'createJobListing',
-    // Other component options and code
-data() {
-        return {
-            filteredResults:[], //response.data.data.bookings is here
-            searchKeyword: '', // Add a data property for search keyword
-            NewRoleName: '', // the new role name
-            NewDepartment: '', //the new department
-            NewDescription: '', // the new Role Description
-            NewRequirements: '', // the new Role Requirements
-            NewDeadline: '', // the new Deadline
-            NewAvailability: '', // the avaiability for new role
-            NewRoleSkills: '', // the skills for the new role
-
-        };
-    },
-    methods: {
-        fetchroles() {
-            axios.get("http://127.0.0.1:5000/roles/get_all_roles")
-            .then(response => {
-                this.filteredResults = response.data.data.roles_with_details
-                console.log(this.filteredResults)
-            })
-            .catch(error => {
-                console.error("There was an error fetching the data:", error);
-            });
-        },
-        test() {
-            console.log(this.NewDeadline)
-            console.log(this.NewDepartment)
-            console.log(this.NewDescription)
-            console.log(this.NewRequirements)
-            console.log(this.NewRoleName)
-            console.log(this.TodaysDate)
-        },
-        createrole() {
-            const inputDateString = this.NewDeadline
-            
-            // Get the current date
-            const today = new Date();
-
-            // Get the year, month, and day components
-            const year = today.getFullYear();
-            const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-            const day = String(today.getDate()).padStart(2, '0');
-
-            // Format the date as YYYY-MM-DD
-            const formattedDateToday = `${year}-${month}-${day}`;
-
-            const formattedSkills = this.NewRoleSkills.split(','); 
- 
-
-            // Prepare the role data to send in the request body
-            const roleData = {
-            Role_Name: this.NewRoleName,
-            Date_Posted: formattedDateToday,
-            App_Deadline: inputDateString,
-            Role_Department: this.NewDepartment, 
-            Role_Description: this.NewDescription, 
-            Role_Requirements: this.NewRequirements,
-            Availability: this.NewAvailability,
-            Role_Skills: formattedSkills
-
-            };
-
-            // Send the PUT request
-            axios.post('http://127.0.0.1:5000/role/create', roleData)
-            .then(response => {
-                // Handle the successful response here
-                console.log('Role created:', response.data);
-            })
-            .catch(error => {
-                // Handle any errors here
-                console.error('Error creating role:', error);
-            });
-        },
-    performSearch(payload) {
-        if (payload) {
-        const { keyword, selectedDepartments, selectedSkills } = payload;
-
-        // Your filtering logic here based on selected departments, skills, and keyword
-        const filteredResults = this.jobListings.filter((job) => {
-            const hasSelectedDepartment =
-            selectedDepartments.length === 0 ||
-            selectedDepartments.includes(job.department);
-            const hasSelectedSkills =
-            selectedSkills.length === 0 ||
-            selectedSkills.some((selectedSkill) =>
-                job.skills.includes(selectedSkill)
-            );
-            const keywordMatch = job.title
-            .toLowerCase()
-            .includes(keyword.toLowerCase());
-
-            return hasSelectedDepartment && hasSelectedSkills && keywordMatch;
-        });
-        this.filteredResults = filteredResults;
-        }
-        },
-    },
-
-    watch: {
-        // Watch for changes to the searchKeyword and trigger performSearch
-        searchKeyword: function() {
-            this.performSearch(); // Call the performSearch method when searchKeyword changes
-        },
-    },
-    mounted() {
-        // Use axios to fetch data from Flask API
-        this.fetchroles();
-    },
+  name: 'createJobListing',
+  data() {
+    return {
+      filteredResults: [],
+      filteredResults2: [],
+      allskills: [],
+      NewRoleName: '',
+      NewDepartment: '',
+      NewDescription: '',
+      NewDeadline: '',
+      NewRoleSkills: '',
+      selectedSkills: [],
     };
+  },
+  methods: {
+    fetchroles() {
+      axios.get(`${server.baseURL}/roles/get_all_roles`)
+        .then(response => {
+          this.filteredResults = response.data.data.roles_with_details;
+          for (const job of this.filteredResults) {
+            if (!this.filteredResults2.includes(job.Role_Department)) {
+                this.filteredResults2.push(job.Role_Department);
+            }
+          }
+          console.log(this.filteredResults);
+        })
+        .catch(error => {
+          console.error("There was an error fetching the data:", error);
+        });
+    },
+    fetchskills() {
+      axios.get(`${server.baseURL}/skills/get_all_skills`)
+        .then(response => {
+          this.allskills = response.data.data.skill_names;
+          console.log(this.allskills);
+        })
+        .catch(error => {
+          console.error("There was an error fetching the data:", error);
+        });
+    },
+     test() {
+      console.log(this.NewDeadline);
+      console.log(this.NewDepartment);
+      console.log(this.NewDescription);
+      console.log(this.NewRoleName);
+    },
+    createrole() {
+      const inputDateString = this.NewDeadline;
+
+      // Get the current date
+      const today = new Date();
+
+      // Get the year, month, and day components
+      const year = today.getFullYear();
+      const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+      const day = String(today.getDate()).padStart(2, '0');
+
+      // Format the date as YYYY-MM-DD
+      const formattedDateToday = `${year}-${month}-${day}`;
+
+      // Prepare the role data to send in the request body
+      const roleData = {
+        Role_Name: this.NewRoleName,
+        Role_Department: this.NewDepartment,
+        Date_Posted: formattedDateToday,
+        App_Deadline: inputDateString,
+        Role_Description: this.NewDescription,
+        Role_Skills: this.selectedSkills,
+      };
+
+      // Send the POST request to create a role
+      axios.post(`${server.baseURL}/role/create`, roleData)
+        .then(response => {
+          // Handle the successful response here
+          console.log('Role created:', response.data);
+        })
+        .catch(error => {
+          // Handle any errors here
+          console.error('Error creating role:', error);
+        });
+    },
+  },
+  mounted() {
+    // Use axios to fetch data from Flask API
+    this.fetchroles();
+    this.fetchskills();
+  },
+};
 </script>
+
 
 
 
