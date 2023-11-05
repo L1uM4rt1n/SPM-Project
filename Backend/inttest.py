8edf33d6f67f599d4334b0111bddbe5c9412ba3b
oreoanytime <adrianustw.2021@scis.smu.edu.sg>
2023-11-05 19:56:17 +0800

    T05 -Successfully fixed sqlite connection
    inttest.py will no longer be dependent on main db

--- Backend/inttest.py
+++ Backend/inttest.py
@@ -1,32 +1,63 @@
 import sqlite3
 import unittest
-import os
-from app import app
+import os,sys
+from app import app, db
 import json  # Don't forget to import the json module
+
+# os.environ['FLASK_ENV'] = 'testing'
+
 
 class IntegrationTest(unittest.TestCase):
     def setUp(self):
         # Create a test database by executing the SQL script
         # Ensure the script is located in the same directory as this test script
-        test_db_filename = 'test.db'
+        test_db_filename = 'instance/test.db' 
         script_filename = 'test.sql'
-
+        print(test_db_filename)
+        print("Current working directory:", os.getcwd())
         # Use SQLite to execute the SQL script
+
+        # Set Flask configuration to use the test database
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///'
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+        
+        with app.app_context():
+            db.create_all()
+
+        
         conn = sqlite3.connect(test_db_filename)
         cursor = conn.cursor()
         with open(script_filename, 'r') as script_file:
             cursor.executescript(script_file.read())
         conn.commit()
         conn.close()
+ 
 
-        # Set Flask configuration to use the test database
-        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{test_db_filename}'
-        app.config['TESTING'] = True
-        self.app = app.test_client()
 
     def tearDown(self):
-        # Cleanup the test database
-        os.remove('test.db')
+        # Clear data from the test database
+        test_db_filename = 'instance/test.db'
+        conn = sqlite3.connect(test_db_filename)
+        cursor = conn.cursor()
+
+        # Clear data from all the tables
+        tables = [
+            'Staff_Role_Apply',
+            'Staff_Skill',
+            'Role_Skill',
+            'Role',
+            'Skill',
+            'Staff',
+            'Access_Control'
+        ]
+
+        for table_name in tables:
+            cursor.execute(f'DELETE FROM {table_name};')
+        conn.commit()
+        conn.close()
+
+################ LOGIN ##################################################
 
     def test_successful_login_staff(self):
         # Send a POST request to the login endpoint
@@ -91,5 +122,37 @@ class IntegrationTest(unittest.TestCase):
         self.assertEqual(response.status_code, 401)
         self.assertEqual(data["message"], "Staff has no HR Rights")
 
+# ################ STAFF ##################################################
+
+    def test_get_all_roles(self):
+
+        # Send a GET request to the /roles/get_all_roles endpoint
+        response = self.app.get('/roles/get_all_roles')
+        data = json.loads(response.data)
+
+        # Check if the response code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the response contains the expected JSON structure
+        self.assertIn("code", data)
+        self.assertEqual(data["code"], 200)
+        self.assertIn("data", data)
+        self.assertIn("roles_with_details", data["data"])
+
+        # Check if the response data includes role details and skills
+        roles_with_details = data["data"]["roles_with_details"]
+        self.assertTrue(isinstance(roles_with_details, list))
+        self.assertTrue(len(roles_with_details) > 0)
+        for role in roles_with_details:
+            self.assertIn("Role_ID", role)
+            self.assertIn("Role_Name", role)
+            self.assertIn("Role_Department", role)
+            self.assertIn("Role_Description", role)
+            self.assertIn("Date_Posted", role)
+            self.assertIn("App_Deadline", role)
+
+    
+
+        
 if __name__ == '__main__':
     unittest.main()
--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,20 @@ from datetime import datetime
 import logging
 import sqlite3
 
+
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/skills_based_role_portal'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root:@localhost:3306/skills_based_role_portal'
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root:@localhost:3306/skills_based_role_portal'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8809/skills_based_role_portal'
+
+
+
+if os.environ.get('FLASK_ENV') == 'testing': 
+    database_uri = 'sqlite:///test.db'
+else:
+    database_uri = 'mysql+mysqlconnector://root:@localhost:3306/skills_based_role_portal'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(24)
