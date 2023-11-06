2b1690e0f4bb25ce64b0eb1838e24ddf57e16e64
oreoanytime <adrianustw.2021@scis.smu.edu.sg>
2023-11-07 03:25:26 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root:@localhost:3306/skills_based_role_portal'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8809/skills_based_role_portal'
 
+
 if __name__ == 'main':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + \
                                             'root:' + \
